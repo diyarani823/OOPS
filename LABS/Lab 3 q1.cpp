cf55f38b6c65038651bd100d67a9691d2f003ba7
diyarani823 <diyarani132@gmail.com>
2026-02-03 00:23:16 +0500

    .

---
+++ LABS/Lab 3 q1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    int *real;
+    int *imag;
+
+public:
+    Complex(int r, int i) {
+        real = new int;
+        imag = new int;
+        *real = r;
+        *imag = i;
+    }
+    // Copy Constructor (Deep Copy)
+    Complex(const Complex &obj) {
+        real = new int;
+        imag = new int;
+        *real = *(obj.real);
+        *imag = *(obj.imag);
+    }
+    void display() {
+        cout << "Complex Number: " << *real << " + " << *imag << "i" << endl;
+    }
+
+    // Destructor
+    ~Complex() {
+        delete real;
+        delete imag;
+    }
+};
+
+int main() {
+    Complex c1(3, 4);      // Original object
+    Complex c2 = c1;      // Copy constructor called
+
+    cout << "Object 1: ";
+    c1.display();
+
+    cout << "Object 2 (Copied): ";
+    c2.display();
+
+    return 0;
+}
