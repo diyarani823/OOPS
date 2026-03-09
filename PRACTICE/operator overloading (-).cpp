e171048c938cf5d9f8add51f7d78259337019a78
diyarani823 <diyarani132@gmail.com>
2026-03-10 04:49:03 +0500

    Create operator overloading (-).cpp

---
+++ PRACTICE/operator overloading (-).cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class complex{
+	public:
+		int real,img;
+		complex(int i,int r)
+		{
+			real=i;
+			img=r;
+		}
+		complex operator -(const complex& hello )
+		{
+			complex temp(real-hello.real,img-hello.img);
+			return temp;
+		}
+		void display()
+		{
+			cout<<real<<"-"<<img<<"i"<<endl;
+		}
+};
+int main()
+{
+	complex c1(24,26),c2(30,4);
+	complex c3=c1-c2;
+	c3.display();
+}
+
