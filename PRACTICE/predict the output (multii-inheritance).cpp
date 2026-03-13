be415074c59ef20e45ab1139b22c242e4e4aff5a
diyarani823 <diyarani132@gmail.com>
2026-03-13 23:57:59 +0500

    Create predict the output (multii-inheritance).cpp

---
+++ PRACTICE/predict the output (multii-inheritance).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class A{
+public:
+    A(int x){ cout<<"A"<<x<<" "; }
+};
+
+class B: public A{
+public:
+    B(int x): A(x+1){ cout<<"B"<<x<<" "; }
+};
+
+class C: public A{
+public:
+    C(int x): A(x+2){ cout<<"C"<<x<<" "; }
+};
+
+class D: public B, public C{
+public:
+    D(int x): B(x+3), C(x+4){ cout<<"D"<<x<<" "; }
+};
+
+int main(){
+    D obj(1);
+}
