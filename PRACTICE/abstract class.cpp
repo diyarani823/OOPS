4cbaa4d24bd9ed2190284d9ef854eec5db8cc4d4
diyarani823 <diyarani132@gmail.com>
2026-03-15 05:55:48 +0500

    Create abstract class.cpp

---
+++ PRACTICE/abstract class.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class shape{
+	public:
+		virtual void hi()=0;
+};
+class circle:public shape{
+	public:
+		void hi(){
+		cout<<"im circle";
+		}
+};
+int main()
+{
+	circle c1;
+	c1.hi();
+}
