0472efaa88c3c44bea4108bb5563810aa48540a0
diyarani823 <diyarani132@gmail.com>
2026-03-17 05:12:06 +0500

    Create inheritance+constructor+overloading+over riding.cpp

---
+++ PRACTICE/inheritance+constructor+overloading+over riding.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class shape{
+public:
+    shape(){
+        cout<<"Shape constructor"<<endl;
+    }
+
+    void display(int x){
+        cout<<"Number: "<<x<<endl;
+    }
+
+    virtual void draw(){
+        cout<<"Drawing Shape"<<endl;
+    }
+};
+
+class circle : public shape{
+public:
+
+    circle(){
+        cout<<"Circle constructor"<<endl;
+    }
+
+    void display(double x){
+        cout<<"Double number: "<<x<<endl;
+    }
+
+    void draw(){
+        cout<<"Drawing Circle"<<endl;
+    }
+};
+
+int main()
+{
+    circle c;
+
+    c.display(10);
+    c.display(5.5);
+
+    c.draw();
+}
