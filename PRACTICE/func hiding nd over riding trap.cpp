495244bafeff3e884f8d3980c4d0c8a9b1abbe08
diyarani823 <diyarani132@gmail.com>
2026-03-18 22:32:05 +0500

    Create func hiding nd over riding trap.cpp

---
+++ PRACTICE/func hiding nd over riding trap.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class Base {
+public:
+    virtual void show() {
+        cout << "Base show\n";
+    }
+
+    void display() {
+        cout << "Base display\n";
+    }
+};
+
+class Derived : public Base {
+public:
+    void show(int x) {   // different parameter
+        cout << "Derived show " << x << endl;
+    }
+
+    void display() {
+        cout << "Derived display\n";
+    }
+};
+
+int main() {
+    Derived d;
+    d.display();
+
+    // d.show(); ? ERROR
+    d.show(10);
+}
