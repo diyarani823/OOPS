3c2fe05092fc971b6f170350543481b3d8a3e1ba
diyarani823 <diyarani132@gmail.com>
2026-03-20 04:55:44 +0500

    Update mixed concepts of oop.cpp

--- PRACTICE/mixed concepts of oop.cpp
+++ PRACTICE/mixed concepts of oop.cpp
@@ -4,22 +4,22 @@ using namespace std;
 class Base {
 public:
     Base() {
-        cout << "Base Constructor\n";
+        cout <<"Base Constructor\n";
     }
 
     virtual void show() {
-        cout << "Base Show\n";
+        cout <<"Base Show\n";
     }
 
     virtual ~Base() {
-        cout << "Base Destructor\n";
+        cout <<"Base Destructor\n";
     }
 };
 
 class Derived : public Base {
 public:
     Derived() {
-        cout << "Derived Constructor\n";
+        cout <<"Derived Constructor\n";
     }
 
     void show() {
