dd3dd1eb8b397041f8a5e16ad95527dcce22c29f
diyarani823 <diyarani132@gmail.com>
2026-03-22 20:10:44 +0500

    Update over ridinggg.cpp

--- PRACTICE/over ridinggg.cpp
+++ PRACTICE/over ridinggg.cpp
@@ -1,20 +1,17 @@
 #include<iostream>
 using namespace std;
-
 class shape{
 public:
     virtual void draw(){
         cout<<"Drawing shape"<<endl;
     }
 };
-
 class circle : public shape{
 public:
     void draw(){
         cout<<"Drawing circle"<<endl;
     }
 };
-
 int main(){
     circle c;
     c.draw();
