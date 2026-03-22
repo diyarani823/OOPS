6e340e4b4353eab20b948d8f6a645fc081ebbc77
diyarani823 <diyarani132@gmail.com>
2026-03-22 20:10:35 +0500

    Update Overriding using Base Class Pointer (Important Concept).cpp

--- PRACTICE/Overriding using Base Class Pointer (Important Concept).cpp
+++ PRACTICE/Overriding using Base Class Pointer (Important Concept).cpp
@@ -1,25 +1,21 @@
 #include<iostream>
 using namespace std;
-
 class animal{
 public:
     virtual void sound(){
         cout<<"Animal makes sound"<<endl;
     }
 };
-
 class dog : public animal{
 public:
     void sound(){
         cout<<"Dog barks"<<endl;
     }
 };
-
 int main()
 {
     animal* a;
     dog d;
-
     a = &d;
     a->sound();
 }
