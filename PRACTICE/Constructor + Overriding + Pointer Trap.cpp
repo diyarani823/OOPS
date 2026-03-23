8ab04f2edc43984282f174c80a761215d5f1b36b
diyarani823 <diyarani132@gmail.com>
2026-03-23 10:45:56 +0500

    Update Constructor + Overriding + Pointer Trap.cpp

--- PRACTICE/Constructor + Overriding + Pointer Trap.cpp
+++ PRACTICE/Constructor + Overriding + Pointer Trap.cpp
@@ -1,28 +1,23 @@
 #include<iostream>
 using namespace std;
-
 class Base {
 public:
     Base() {
         cout << "Base Constructor\n";
     }
-
     virtual void show() {
         cout << "Base Show";
     }
 };
-
 class Derived : public Base {
 public:
     Derived() {
         cout << "Derived Constructor";
     }
-
     void show() {
         cout << "Derived Show";
     }
 };
-
 int main() {
     Base *b = new Derived();
     b->show();
