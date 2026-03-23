83dd657dbc305bbc2cbb0a94b50be393d79b63dc
diyarani823 <diyarani132@gmail.com>
2026-03-23 10:46:00 +0500

    Update func hiding nd over riding trap.cpp

--- PRACTICE/func hiding nd over riding trap.cpp
+++ PRACTICE/func hiding nd over riding trap.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-
 class Base {
 public:
     virtual void show() {
@@ -20,7 +19,6 @@ public:
         cout << "Derived display\n";
     }
 };
-
 int main() {
     Derived d;
     d.display();
