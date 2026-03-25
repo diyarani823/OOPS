928eb1e1be82331abded84a61d983824f8da716d
diyarani823 <diyarani132@gmail.com>
2026-03-26 02:43:18 +0500

    Update abstract class.cpp

--- PRACTICE/abstract class.cpp
+++ PRACTICE/abstract class.cpp
@@ -1,20 +1,17 @@
 #include<iostream>
 using namespace std;
 class shape{
-	public:
-		virtual void hi()=0;
+public:
+virtual void hi()=0;
 };
 class circle:public shape{
-	public:
-		void hi(){
-		cout<<"im circle";
-		}
-		
+public:
+void hi(){
+cout<<"im circle";
+}		
 };
 int main()
 {
-	
 	circle c1;
 	c1.hi();
-	
 }
