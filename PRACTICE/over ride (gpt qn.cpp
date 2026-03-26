27c6346bbb140a514f6611022ade49f9407a13e0
diyarani823 <diyarani132@gmail.com>
2026-03-26 21:18:50 +0500

    Update over ride (gpt qn.cpp

--- PRACTICE/over ride (gpt qn.cpp
+++ PRACTICE/over ride (gpt qn.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 class shape{
+	
 public:
 virtual	void area(){
 cout<<"area of the shape";
+
 }
 };
 class rectangle:public shape {
@@ -13,6 +15,7 @@ cout<<"area of rectangle";
 }
 };
 int main()
+
 {
 shape s;
 s.area();
