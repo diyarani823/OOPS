444bca3a04db59840daa5c370bfa63c6d4bdfc94
diyarani823 <diyarani132@gmail.com>
2026-05-20 23:29:08 +0500

    Update lab 11 q1.cpp

--- PRACTICE/lab 11 q1.cpp
+++ PRACTICE/lab 11 q1.cpp
@@ -1,52 +1,71 @@
 #include<iostream>
 using namespace std;
-class passwordlengthexception{
-	public:
-		const char* what()
-		{
-			return "password cannot have less than 6 characters.";
-		}
-			
+
+class passwordlengthexception
+{
+public:
+    const char* what()
+    {
+        return "password cannot have less than 6 characters.";
+    }
 };
+
 class passwordDigitException
 {
-	public:
-		const char* what()
-		{
-			return"password must contain atleast one digit;";
-		}
+public:
+    const char* what()
+    {
+        return "password must contain at least one digit.";
+    }
 };
+
 int main()
 {
-	string username,password;
-	cout<<"enter your username";
-	cin>>username;
-	cout<<"enter password";
-	try{
-		if(password.length()<6){
-			passwordlengthexception e;
-			throw e;
-		}
-		int found=-1;
-		for(int i=0;i<password.length();i++)
-		{
-			if(password[i]>=48 && password[i]<=57)
-			found=1;
-			break;
-		}
-	}
-	if (found==-1)
-	{passwordlengthexception e;
-			throw e;
-		}
-	
-	cout<<"login successful"<<endl;
-	cout<<"username"<<username<<endl<<"password"<<password<<endl;	
-	}
-	catch(passwordlengthexception &e){
-		cout<<e.what;
-	}
-catch(passwordDigitException &e){
-		cout<<e.what;
-	}
+    string username, password;
+
+    cout << "Enter your username: ";
+    cin >> username;
+
+    cout << "Enter password: ";
+    cin >> password;
+
+    try
+    {
+        if(password.length() < 6)
+        {
+            passwordlengthexception e;
+            throw e;
+        }
+
+        int found = -1;
+
+        for(int i = 0; i < password.length(); i++)
+        {
+            if(password[i] >= '0' && password[i] <= '9')
+            {
+                found = 1;
+                break;
+            }
+        }
+
+        if(found == -1)
+        {
+            passwordDigitException e;
+            throw e;
+        }
+
+        cout << "Login successful" << endl;
+        cout << "Username: " << username << endl;
+        cout << "Password: " << password << endl;
+    }
+
+    catch(passwordlengthexception &e)
+    {
+        cout << e.what();
+    }
+
+    catch(passwordDigitException &e)
+    {
+        cout << e.what();
+    }
 }
